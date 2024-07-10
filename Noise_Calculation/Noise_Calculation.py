3a13ee91e0522ba0caeb40bb3c1739ed6ae8346c
flaviuromanescu <romanescuflaviu@gmail.com>
2024-07-10 11:21:55 +0200

    Update Noise_Calculation.py

--- Noise_Calculation/Noise_Calculation.py
+++ Noise_Calculation/Noise_Calculation.py
@@ -1,37 +1,35 @@
-# %%
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as p
 
-# %%
-Planck_Noise = np.array(pd.read_csv(r'M:\UNI\Bachelor_Project\Fisher_Forecast_VS_Code\Noise_Calculation\Planck_Noise.csv', header = None))
-CMBS4_Initial_Noise = np.array(pd.read_csv(r'M:\UNI\Bachelor_Project\Fisher_Forecast_VS_Code\Noise_Calculation\CMB_S4_Initial_Noise.csv', header = None))
+# Declare home PATH (ex: M:\Folder1\Folder2 (Windows) or /Folder1/Folder2 (Linux))
+# If working on Windows, change path separators to '\' instead of '/'
+PATH = r'insert_path_here'
 
-# %%
+# Import noise data
+Planck_Noise = np.array(pd.read_csv(PATH + r'/Noise_Calculation/Planck_Noise.csv', header = None))
+CMBS4_Initial_Noise = np.array(pd.read_csv(PATH + r'/Noise_Calculation/CMB_S4_Initial_Noise.csv', header = None))
+
+# Generate array for the final noise
 CMBS4_Final_Noise = np.zeros(Planck_Noise.shape)
 
+# Transforming from muK to K
 Planck_Noise[:,1] *= 10**(-12)
 Planck_Noise[:,2] *= 10**(-12)
 CMBS4_Initial_Noise[:,1] *= 10**(-12)
 CMBS4_Initial_Noise[:,2] *= 10**(-12)
 
+# Computing noise
 for i in range(len(Planck_Noise[:,0])):
 
+    # Pure Planck Noise
     CMBS4_Final_Noise[i,0] = int(Planck_Noise[i,0])
+    
+    # T Noise
     CMBS4_Final_Noise[i,1] = (Planck_Noise[i,1]**(-1) + CMBS4_Initial_Noise[i,1]**(-1)) ** (-1)
+
+    # E Noise
     CMBS4_Final_Noise[i,2] = (Planck_Noise[i,2]**(-1) + CMBS4_Initial_Noise[i,2]**(-1)) ** (-1)
 
+# Saving to csv
 df = pd.DataFrame(CMBS4_Final_Noise)
 df.to_csv('CMB_S4_Final_Noise.csv', header = None, index = None)
-
-
-# %%
-fig = plt.figure(figsize=(10,6))
-
-plt.plot(CMBS4_Final_Noise[:,0], CMBS4_Final_Noise[:,1], color = 'blue', alpha = 0.6)
-plt.plot(CMBS4_Final_Noise[:,0], Planck_Noise[:,1], color = 'red', alpha = 0.6)
-plt.plot(CMBS4_Final_Noise[20:,0], CMBS4_Initial_Noise[20:,1], color = 'green', alpha = 0.6)
-plt.grid()
-plt.show()
-
-
